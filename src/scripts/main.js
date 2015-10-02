72c7fa9a8c47c94411d67c691f1abfcf3c7417ec
Matheus Brasil <matheus.brasil10@gmail.com>
2015-10-02 02:21:16 -0300

    Remove unused dependencies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,10 +13,6 @@ Reveal.initialize({
     { src: 'vendor/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
     { src: 'vendor/reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
     { src: 'vendor/reveal.js/plugin/notes/notes.js', async: true },
-    { src: 'vendor/reveal.js/plugin/math/math.js', async: true },
-    { src: 'vendor/reveal.js/plugin/print-pdf/print-pdf.js', async: true },
-    { src: 'vendor/reveal.js/plugin/zoom-js/zoom.js', async: true },
-    { src: 'vendor/chartist/dist/chartist.min.js', async: true },
-    { src: 'vendor/chartist/dist/vivus.min.js', async: true }
+    { src: 'vendor/reveal.js/plugin/zoom-js/zoom.js', async: true }
   ]
 });
